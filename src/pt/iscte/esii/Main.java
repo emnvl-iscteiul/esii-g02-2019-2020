33dfad98ee7feae1ef24cce33faae3fc5355bb10
emnvl <emnvl@iscte-iul.pt>
2020-03-07 17:33:29 +0000

    Added a method to return the computer name before the 'Hello, World!'

--- src/pt/iscte/esii/Main.java
+++ src/pt/iscte/esii/Main.java
@@ -1,7 +1,36 @@
 package pt.iscte.esii;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 public class Main {
+
+    /**
+     * Main entry point for the application
+     *
+     * @param args arguments passed in the command-line
+     */
     public static void main(String[] args) {
+        String name = getMachineName();
+        if (!name.equals("")) System.out.print(getMachineName() + " says: ");
         System.out.println("Hello, world!");
     }
+
+    /**
+     * Retuns the hostname for the local machine
+     *
+     * @return The hostname for this machine
+     */
+    private static String getMachineName(){
+        String hostname = "";
+        try {
+            InetAddress addr;
+            addr = InetAddress.getLocalHost();
+            hostname = addr.getHostName();
+        } catch (UnknownHostException ex) {
+            System.out.println("Hostname can not be resolved");
+        }
+        return hostname;
+    }
+
 }
